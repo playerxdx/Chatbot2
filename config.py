c1e046575d01c7c3a5fd6cc0a3de9ff1f5f55267
playerxdx <128796714+playerxdx@users.noreply.github.com>
2025-01-18 15:23:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 27353035))
+API_ID = int(getenv("API_ID", "27353035"))
 API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
 OWNER_ID = int(getenv("OWNER_ID", "6651270559"))
 STRING_SESSION = getenv("STRING_SESSION", "1BVtsOKEBu0rCY1YjqWi8Cr-YsPuqlrT3QW_id-3T-ImK3_iqgzQqlrw_RxjwWiaR5p1lbgqp_6beteIJyPoAlJZqLONB7bYukv_8OjB1hwV_e9MHO7Y6JZXgY9jJ6WCrZ7z3iBmjAdkki6AbO3_5L0kwnxtKgpWKaVwFRuwgfhywlVveHk3zho5Kj2ecoZrsB2eJwMPqTNA0lyTjKY6CY_BJF-ZlrTMYRocxEuA0D6AW2tS_RVkpudyKngJQaPZy553qsjRpcWIfS36zjz2DU96nAhH2JarlRB-7CjMVSGoBeoNUiHTz28X614hnwf8P3b7VXxFbi9DwBu6sE57bXAWutw2VuNs=")
