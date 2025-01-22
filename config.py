2d5ebb686d4102348f83c13fd8baf7c47f713aa4
playerxdx <128796714+playerxdx@users.noreply.github.com>
2025-01-22 09:34:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", "27353035"))
 API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
-OWNER_ID = int(getenv("OWNER_ID", "6651270559"))
+OWNER_ID = int(getenv("OWNER_ID", "7100217891"))
 STRING_SESSION = getenv("STRING_SESSION", "BQGqxQoAWMvTJMXAq7q_l6barGU3yxdRbTkJLLf7Gt-7aYiXRjRcG-nMNWxip6pSosKMC7vi69mK538r4hJmDoeUwo-kAutmpUPMg07RSu1W1oEHtEU1MWRRJ5MvMVt_gjgBTni33iWJnEJ9hcQdlhtM_BrnE8dpqQfP3swyNALaciMZf24SA-Yu5oIpl8TVdoZBiogpPDuxA5YM56zsZDMxGAeNbb0RKmd6rseyDAuf3FmCv6_v72XbzSvirPJMWsY06niBNIVbvroWvF4GS2doOubuTAec0ha-4u-4kwbr55SMQopYndX7_uMTlnRlg2zfYOyzt0R5Bw8iFgvwM_nM7np_HAAAAAGQszlEAA")
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://aakashxd:aakashxd@cluster0.rnnus.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 SUPPORT_GRP = getenv("SUPPORT_GRP", "AakashPosts")
